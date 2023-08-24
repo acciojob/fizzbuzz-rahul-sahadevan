6d7e8141a237ccbc7177874e665557fb91d65b78
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-24 11:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let n = 100;
+
+for(let i = 1;i<=100;i++){
+    if(i % 3 == 0){
+		console.log('Fizz');
+        // alert("fizz");
+    }
+    else if(i % 5 == 0){
+		console.log("Buzz");
+        // alert("buzz");
+    }
+    else if(i % 3 == 0 && i%5 == 0){
+		console.log("FizzBuzz");
+        // alert("fizz-buzz");
+    }
+    else{
+		console.log(i);
+        // alert(i);
+    }
+}
